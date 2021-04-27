bd7e872269df5562b925dd03f8da7d33c13cf76e
Wiper-R <rshivang12345@gmail.com>
2021-04-27 10:49:36 +0530

    Added General Commands

---
+++ cogs/general.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+from datetime import datetime
+import humanize
+
+
+def setup(bot):
+    bot.add_cog(General(bot))
+
+
+class General(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        if not hasattr(self.bot, "uptime"):
+            self.bot.uptime = datetime.utcnow()
+
+    @commands.command()
+    async def ping(self, ctx: commands.Context):
+        return await ctx.send(f"Pong! **{self.bot.latency * 1000:.0f} ms**")
+
+    @commands.command()
+    async def uptime(self, ctx: commands.Context):
+        humanized = humanize.naturaldelta(datetime.utcnow() - self.bot.uptime)
+        return await ctx.send(f"Uptime: **{humanized}**")
