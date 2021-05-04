26bb721f20f1fa8f22740142d256fc7dc61816ef
Wiper-R <rshivang12345@gmail.com>
2021-05-04 10:58:17 +0530

    Added Some More Commands

--- migrations/models/4_20210430091545_None.sql
+++ migrations/models/4_20210430091545_None.sql
@@ -13,11 +13,15 @@ CREATE TABLE IF NOT EXISTS "voice_channels" (
     "type" VARCHAR(10) NOT NULL  DEFAULT 'normal'
 );
 COMMENT ON COLUMN "voice_channels"."type" IS 'normal: normal\nsequential: sequential\npredefined: predefined\ncloned: cloned';
-CREATE TABLE IF NOT EXISTS "member_voice" (
+CREATE TABLE IF NOT EXISTS "member_config" (
     "id" BIGSERIAL NOT NULL PRIMARY KEY,
     "name" VARCHAR(100),
     "limit" INT NOT NULL  DEFAULT 0,
-    "bitrate" INT
+    "bitrate" INT NOT NULL  DEFAULT 64000
+);
+CREATE TABLE IF NOT EXISTS "guild_config" (
+    "id" BIGSERIAL NOT NULL PRIMARY KEY,
+    "prefix" VARCHAR(3) NOT NULL
 );
 CREATE TABLE IF NOT EXISTS "voice_config" (
     "id" BIGSERIAL NOT NULL PRIMARY KEY,
